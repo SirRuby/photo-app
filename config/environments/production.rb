e7630afa5ec6fe6516396e1e23e012402bd3d46b
SirRuby <staffordharrison@protonmail.com>
2020-05-05 15:41:49 +0700

    add functionality for email verification and update ruby version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'staffords-photo-app.herokuapp.com/', :protocol => 'https' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
